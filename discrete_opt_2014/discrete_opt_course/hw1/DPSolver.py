e21c5715f2092110a2bbaef8a1612733e336680c
ronin2448 <ronin2448@gmail.com>
2014-04-07 02:23:32 -0500

    first upload of hw1

---
+++ discrete_opt_2014/discrete_opt_course/hw1/DPSolver.py
@@ -0,0 +1,38 @@
+'''
+Created on Mar 10, 2014
+
+@author: Domingo
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''  
+    
+    def solveIt(self, capacity, items):
+        
+        self.weightTaken = 0
+        self.itemsToPic = items
+        
+        output = ""
+        return output
+    
+    def getComputeVal(self, maxCap , indexOfItemsToSelect):
+        if indexOfItemsToSelect == 0:
+            return 0
+        else:
+            itemToSelect = self.itemsToPic[indexOfItemsToSelect]
+            weightOfExtraItem = itemToSelect.weight
+            
+            if w <= maxCap:
+                return max( self.getComputeVal(maxCap, indexOfItemsToSelect-1) , v + self.getComputeVal(maxCap-w, indexOfItemsToSelect-1) )
+            else:
+                return self.getComputeVal(maxCap, indexOfItemsToSelect-1)
+
---
+++ discrete_opt_2014/discrete_opt_course/hw1/dynamicProg.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple
+from operator import attrgetter
+
+Item = namedtuple("Item", ['index', 'value', 'weight'])
+
+def parse_data(input_data):
+    # parse the input
+    lines = input_data.split('\n')
+
+    firstLine = lines[0].split()
+    item_count = int(firstLine[0])
+    capacity = int(firstLine[1])
+
+    items = []
+
+    for i in range(1, item_count+1):
+        line = lines[i]
+        parts = line.split()
+        items.append(Item(i-1, int(parts[0]), int(parts[1])))
+    
+    return (capacity, items)
+
+def solve_it_orig(input_data):
+    # Modify this code to run your optimization algorithm
+
+    # parse the input
+    lines = input_data.split('\n')
+
+    firstLine = lines[0].split()
+    item_count = int(firstLine[0])
+    capacity = int(firstLine[1])
+
+    items = []
+
+    for i in range(1, item_count+1):
+        line = lines[i]
+        parts = line.split()
+        items.append(Item(i-1, int(parts[0]), int(parts[1])))
+
+    # a trivial greedy algorithm for filling the knapsack
+    # it takes items in-order until the knapsack is full
+    value = 0
+    weight = 0
+    taken = [0]*len(items)
+
+    for item in items:
+        if weight + item.weight <= capacity:
+            taken[item.index] = 1
+            value += item.value
+            weight += item.weight
+    
+    # prepare the solution in the specified output format
+    output_data = str(value) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, taken))
+    return output_data
+
+def solve_it_via_rules(input_data):
+    # Modify this code to run your optimization algorithm
+
+    # parse the input
+    (capacity, items) = parse_data(input_data)
+
+    # a trivial greedy algorithm for filling the knapsack
+    # it takes items in-order until the knapsack is full
+#     value = 0
+#     weight = 0
+#     taken = [0]*len(items)
+#     
+#     sortedItemList = sorted(items, key=attrgetter('value'), reverse=True)
+#     print items
+#     print sortedItemList
+# 
+#     for item in sortedItemList:
+#         if weight + item.weight <= capacity:
+#             taken[item.index] = 1
+#             value += item.value
+#             weight += item.weight
+#     
+#     # prepare the solution in the specified output format
+#     output_data = str(value) + ' ' + str(0) + '\n'
+#     output_data += ' '.join(map(str, taken))
+#     return output_data
+
+#     return solve_via_grabbing_most_valuable_items(capacity, items)
+    return solve_via_grabbing_most_useful_items(capacity, items)
+
+
+
+
+def solve_via_grabbing_most_valuable_items(capacity, items):
+    value = 0
+    weight = 0
+    taken = [0]*len(items)
+    
+    sortedItemList = sorted(items, key=attrgetter('value'), reverse=True)
+    print items
+    print sortedItemList
+
+    for item in sortedItemList:
+        if weight + item.weight <= capacity:
+            taken[item.index] = 1
+            value += item.value
+            weight += item.weight
+    
+    # prepare the solution in the specified output format
+    output_data = str(value) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, taken))
+
+    return output_data
+
+def solve_via_grabbing_most_useful_items(capacity, items):
+    value = 0
+    weight = 0
+    taken = [0]*len(items)
+ 
+    print items   
+    sortedItemList = sorted(items, key= lambda x : ((x.value*1.0)/x.weight)*1.0, reverse=True)
+    print sortedItemList
+ 
+    prevValue = -1
+    for item in sortedItemList:
+        if prevValue == -1:
+            prevValue = (item.value*1.0)/item.weight
+        curVal = (item.value*1.0)/item.weight
+        print curVal
+        if prevValue < curVal:
+            print "ERROR!"
+        else:
+            prevValue =curVal
+            
+            
+        if weight + item.weight <= capacity:
+            taken[item.index] = 1
+            value += item.value
+            weight += item.weight
+    
+    # prepare the solution in the specified output format
+    output_data = str(value) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, taken))
+
+    return output_data
+
+
+def solve_via_DP(capacity, items):
+    output_data = ""
+    return output_data
+
+import sys
+
+if __name__ == '__main__':
+        file_location = './data/ks_30_0'
+        input_data_file = open(file_location, 'r')
+        input_data = ''.join(input_data_file.readlines())
+        input_data_file.close()
+        print "Default"
+        print solve_it_orig(input_data)
+        print "My soloution"
+        print solve_it_via_rules(input_data)
